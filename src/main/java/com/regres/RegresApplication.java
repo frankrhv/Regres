542740a1a708d8586940ce07aaf4f93158807d4e
frankrhv <frank.uni.fiee@gmail.com>
2021-11-13 15:14:00 -0500

    Initial commit

---
+++ src/main/java/com/regres/RegresApplication.java
@@ -0,0 +1,13 @@
+package com.regres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/regres/RegresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
